7839e53cf4b03522fa4f80b87ddffd74f20e8c30
RobyCajulis <rossbrent.cajulis@email.lcup.edu.ph>
2025-09-03 21:47:29 +0800

    new update

--- javascript.js
+++ javascript.js
@@ -20,3 +20,29 @@ themeToggle.addEventListener("click", () => {
     localStorage.setItem("theme", "light");
   }
 });
+
+
+/*
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 80; // offset for header height
+    const sectionHeight = section.offsetHeight;
+
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+*/
